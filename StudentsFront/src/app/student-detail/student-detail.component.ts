8be2287c3b8dc77c1ca6c7d3ae11db62f22a1a78
Radosław Burdziński <radoslaw.burdzinski@beone.pl>
2022-05-25 22:00:04 +0200

    zajecia angular

---
+++ StudentsFront/src/app/student-detail/student-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { StudentService } from '../student.service';
+@Component({
+  selector: 'app-student-detail',
+  templateUrl: './student-detail.component.html',
+  styleUrls: ['./student-detail.component.css']
+})
+export class StudentDetailComponent implements OnInit {
+  student?: Student;
+
+  getStudent(): void {
+    const pathId = this.route.snapshot.paramMap.get('id');
+    if (pathId) {
+    this.studentService.getStudent(+pathId)
+    .subscribe(student => this.student = student);
+    }
+    }
+    save(): void {
+      this.studentService.updateStudent(this.student!)
+      .subscribe(() => this.goBack());
+     }
+     
+
+  constructor(
+    private route: ActivatedRoute,
+    private studentService: StudentService,
+    private location: Location
+   ) {}
+   goBack(): void {
+    this.location.back();
+   }
+   ngOnInit() {
+    this.getStudent();
+    }
+
+}
